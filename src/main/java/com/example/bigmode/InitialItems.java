0415ac5b197e83970ff84ca6fcc6eb1fd93f441d
Cleudeir <cleudeir@jrmendonca.com.br>
2024-06-22 19:46:20 -0300

    up

--- src/main/java/com/example/bigmode/InitialItems.java
+++ src/main/java/com/example/bigmode/InitialItems.java
@@ -1,28 +1,22 @@
 package com.example.bigmode;
 
-import net.minecraft.world.item.Items;
-import net.minecraft.server.MinecraftServer;
+import java.util.HashSet;
+import java.util.UUID;
+
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.server.ServerLifecycleHooks;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
-import java.io.File;
 
 @Mod.EventBusSubscriber(modid = YourMod.MODID)
 public class InitialItems {
-    private static final Set<UUID> receivedInitialItems = new HashSet<>();
+    private HashSet<UUID> items = new HashSet<>();
+  
 
     public InitialItems() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -31,15 +25,15 @@ public class InitialItems {
 
     private void setup(final FMLCommonSetupEvent event) {
         // Initialization code, if needed
-        loadInitialItemsData();
+        items = storage.loadInitialItemsData("initial_data");    
     }
 
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        if (!receivedInitialItems.contains(event.getEntity().getUUID())) {
+        if (!items.contains(event.getEntity().getUUID())) {
             giveInitialItems(event.getEntity());
-            receivedInitialItems.add(event.getEntity().getUUID());
-            saveInitialItemsData();
+            items.add(event.getEntity().getUUID());
+            storage.saveInitialItemsData("initial_data",items);
         }
     }
 
@@ -51,67 +45,5 @@ public class InitialItems {
         player.getInventory().add(new ItemStack(Items.STONE_HOE, 1));
         player.getInventory().add(new ItemStack(Items.GLASS, 64));
         player.getInventory().add(new ItemStack(Items.BREAD, 8));
-    }
-
-    private String getWorldName() {   
-        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
-        if (server != null) {
-            return server.getWorldData().getLevelName();
-        } else {
-            // Handle null server case (example fallback)
-            return "unknown_world";
-        }
-    }
-
-    private void loadInitialItemsData() {
-        String worldName = getWorldName();
-        try {
-            File file = new File("./config/bigmode/initial_items_" + worldName + ".dat"); // Adjust the path as needed
-            if (file.exists()) {
-                FileInputStream fis = new FileInputStream(file);
-                byte[] data = new byte[(int) file.length()];
-                fis.read(data);
-                fis.close();
-                for (int i = 0; i < data.length; i += 16) {
-                    long most = 0;
-                    for (int j = 0; j < 8; j++) {
-                        most <<= 8;
-                        most |= data[i + j] & 0xff;
-                    }
-                    long least = 0;
-                    for (int j = 8; j < 16; j++) {
-                        least <<= 8;
-                        least |= data[i + j] & 0xff;
-                    }
-                    receivedInitialItems.add(new UUID(most, least));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void saveInitialItemsData() {
-        String worldName = getWorldName();
-        try {
-            File file = new File("./config/bigmode/initial_items_" + worldName + ".dat"); // Adjust the path as needed
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            FileOutputStream fos = new FileOutputStream(file);
-            for (UUID uuid : receivedInitialItems) {
-                long most = uuid.getMostSignificantBits();
-                long least = uuid.getLeastSignificantBits();
-                for (int i = 7; i >= 0; i--) {
-                    fos.write((byte) (most >> (8 * i)));
-                }
-                for (int i = 7; i >= 0; i--) {
-                    fos.write((byte) (least >> (8 * i)));
-                }
-            }
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
---
+++ src/main/java/com/example/bigmode/storage.java
@@ -0,0 +1,56 @@
+package com.example.bigmode;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.UUID;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.server.ServerLifecycleHooks;
+
+public class storage {
+    private static Gson gson = new Gson();
+
+    private static String getWorldName() {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        if (server != null) {
+            return server.getWorldData().getLevelName().toLowerCase(); // Ensure consistent naming convention
+        } else {
+            return "unknown_world";
+        }
+    }
+
+    private static Path getConfigFilePath(String name) {
+        return Paths.get("./config/bigmode_" + name + "_" + getWorldName() + ".json");
+    }
+
+    public static HashSet<UUID> loadInitialItemsData(String name) {
+        Path filePath = getConfigFilePath(name);
+        try (FileReader reader = new FileReader(filePath.toFile())) {
+            Type setType = new TypeToken<HashSet<UUID>>() {
+            }.getType();
+            return gson.fromJson(reader, setType);
+        } catch (IOException e) {
+            System.out.println("Error loading initial items data for " + name + ": " + e.getMessage());
+        }
+        return new HashSet<>();    
+    }
+
+    public static void saveInitialItemsData(String name, HashSet<UUID> items) {
+        Path filePath = getConfigFilePath(name);
+        try (FileWriter writer = new FileWriter(filePath.toFile())) {
+            gson.toJson(items, writer);
+        } catch (IOException e) {
+            System.out.println("Error saving initial items data for " + name + ": " + e.getMessage());
+            // Handle more specific exceptions if needed
+        }
+    }
+
+}
